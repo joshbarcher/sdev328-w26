091888936899fe357f0cbddafc6158f3b00cc19a
Josh Archer <jarcher@greenriver.edu>
2026-02-19 10:30:38 -0800

    Added a form

--- api-frontend/public/js/data.js
+++ api-frontend/public/js/data.js
@@ -11,6 +11,24 @@ window.onload = async () => {
     const data = await response.json();
     console.log(data);
 
-    document.querySelector("#output").innerHTML = data[0].title;
+    // document.querySelector("#output").innerHTML = data[0].title;
+    renderOnLoad(data);
 }
 
+function renderOnLoad(data) {
+    for (const record of data) {
+        addTableRow(record);
+    }
+}
+
+function addTableRow(rowData) {
+    const html = `<tr>
+        <td>${rowData.id}</td>
+        <td>${rowData.title}</td>
+        <td>${rowData.franchise}</td>
+        <td>${rowData.genre[0]}</td>
+    </tr>`;
+    
+    const tbody = document.querySelector("#games-list");
+    tbody.innerHTML += html;
+}
