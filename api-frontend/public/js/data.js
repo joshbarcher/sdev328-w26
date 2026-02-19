7fdfeb41466274c80c73efe849f99de7e255329c
Josh Archer <jarcher@greenriver.edu>
2026-02-19 11:05:48 -0800

    Added a POST request.

--- api-frontend/public/js/data.js
+++ api-frontend/public/js/data.js
@@ -1,5 +1,47 @@
 
 window.onload = async () => {
+    await loadData();
+    formSetup();
+}
+
+function formSetup() {
+    const form = document.querySelector("#game-form");
+    form.onsubmit = handleFormSubmit;
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault(); //prevent the form from submitting
+
+    console.log("Submitted");
+
+    const bodyData = readForm();
+
+    //"Submit" the form using fetch()
+    const uri = "http://localhost:8001/api/v1/games/game";
+    const config = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(bodyData)
+    }
+    
+    const response = await fetch(uri, config);
+    const responseBody = await response.json();
+
+    console.log(responseBody);
+}
+
+function readForm() {
+    return {
+        id: document.querySelector("#id").value,
+        title: document.querySelector("#title").value,
+        franchise: document.querySelector("#franchise").value,
+        genre: document.querySelector("#genre").value,
+    }
+}
+
+async function loadData() {
     let uri = 'http://localhost:8001/api/v1/games';
     let config = {
         method: "get",
@@ -28,7 +70,7 @@ function addTableRow(rowData) {
         <td>${rowData.franchise}</td>
         <td>${rowData.genre[0]}</td>
     </tr>`;
-    
+
     const tbody = document.querySelector("#games-list");
     tbody.innerHTML += html;
 }
